3b8cd1f71e8d6342308128e55ac5caf7f8e0554e
Jordan Gilliam <gilliamja.te@gmail.com>
2018-01-31 22:24:04 +0000

    added react-media

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+import Media from 'react-media'
 
 import './index.css'
 
